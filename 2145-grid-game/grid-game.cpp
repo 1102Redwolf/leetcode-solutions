7ee6356075139363a675ab901f21f9ae61918db2
Shubham <svkk5432109876@gmail.com>
2026-07-10 17:04:52 +0530

    Time: 0 ms (100.00%) | Memory: 66.7 MB (51.53%) - LeetSync

---
+++ 2145-grid-game/grid-game.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long gridGame(vector<vector<int>>& grid) {
+
+        long long topSum = 0;
+        long long bottomSum = 0;
+
+        for (int x : grid[0])
+            topSum += x;
+
+        long long ans = LLONG_MAX;
+
+        for (int i = 0; i < grid[0].size(); i++) {
+
+            topSum -= grid[0][i];
+
+            ans = min(ans, max(topSum, bottomSum));
+
+            bottomSum += grid[1][i];
+        }
+
+        return ans;
+    }
+};
